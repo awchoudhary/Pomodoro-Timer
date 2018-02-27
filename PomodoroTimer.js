14d5b8a61fca53c0c72667f8a3ad4f90a0de8375
Awaes Choudhary <acedu77@gmail.com>
2018-02-26 22:06:11 -0600

    Refatored to remove duration and label arrays

--- PomodoroTimer.js
+++ PomodoroTimer.js
@@ -1,14 +1,8 @@
 //Timer object to perform countdown
 var pomodoroTimer = false;
 
-//The length of each Pomodoro round in seconds
-var roundDurations = [1500, 30, 1500, 30, 1500, 30, 1500, 900];
-
-//THe name for each Pomodoro round
-var roundLabels = ["First Round", "Break", "Second Round", "Break", "Third Round", "Break", "Fourth Round", "Break"];
-
 //Keeps track of the current Pomodoro round
-var currentRound = 0;
+var currentRound = 1;
 
 //Indicates if timer is paused
 var paused = false;
@@ -35,59 +29,71 @@ $("document").ready(function(){
 
 function startPomodoroTimer(){
     // Set the date we're counting down to
-    var countDownTime = roundDurations[currentRound];
+    var countdown = getCountdownTime(currentRound);
                     
     // Update the count down every 1 second
     pomodoroTimer = setInterval(function() {
         
         if(!paused){
-            countDownTime = countDownTime - 1;
+            countdown = countdown - 1;
         }                
 
         // Update Timer UI
         $("#timer").removeClass("timerTimeUp");
-        $("#timer").html(getCountdownString(countDownTime));
+        $("#timer").html(getCountdownString(countdown));
                             
         // If the count down is over, clear the timer
-        if (countDownTime < 0) {
+        if (countdown < 0) {
             clearInterval(pomodoroTimer);
             pomodoroTimer = false;
 
             //update the current round
-            if(currentRound == roundDurations.length - 1){
-                currentRound = 0;
-            }
-            else{
-                currentRound = currentRound + 1;
-            }
+            currentRound++;
 
             //set "time up" text and add red text styling
             $("#timer").html("TIME UP");
             $("#timer").addClass("timerTimeUp");
 
             //update button UI
-            $("#timerButton").html("Start " + roundLabels[currentRound] + "<br> (" + roundDurations[currentRound]/60 + " Minutes)");
+            $("#timerButton").html(getButtonLabel(currentRound, getCountdownTime(currentRound)));
             $("#timerButton").removeClass("timerButtonPause").addClass("timerButtonStart");
         }
     }, 1000);
+}
+
+function getButtonLabel(currentRound, countdown){
+    //every even numbered round is a short break.
+    if(currentRound % 2 == 0){
+        return "Start Break <br> (" + countdown/60 + " Minutes)";
+    }
+    //every odd numbered round is a normal round.
+    else{
+        return "Start Round <br> (" + countdown/60 + " Minutes)";
+    }
+}
+
+//get countdown time based on the round number
+function getCountdownTime(currentRound){
+    //every 8 rounds is a long break.
+    if(currentRound % 8 == 0){ 
+        return 90;
+    }
+    //every even numbered round is a short break.
+    else if(currentRound % 2 == 0){
+        return 30;
+    }
+    //every odd numbered round is a normal round.
+    else{
+        return 60;
+    }
 }
 
 //Convert seconds to "MM:ss" formated string
 function getCountdownString(time){
     var minutes = Math.floor(time / 60);
     var seconds = time - (minutes * 60);
-    var minutesString = minutes.toString();
-    var secondsString = seconds.toString();
 
-    if(minutes < 10){
-        minutesString = "0" + minutesString;
-    }
-
-    if(seconds < 10){
-        secondsString = "0" + secondsString;
-    }
-
-    var timeString = minutesString + ":" + secondsString;
+    var timeString = ((minutes < 10) ? "0" : "") + minutes.toString() + ":" + ((seconds < 10) ? "0" : "") + seconds.toString();
 
     return timeString;
 }
