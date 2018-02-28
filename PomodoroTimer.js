d938d5e9251af095e10610975375db91dbd0ecd0
Awaes Choudhary <acedu77@gmail.com>
2018-02-27 23:40:32 -0600

    Settings to adjust round and break durations

--- PomodoroTimer.js
+++ PomodoroTimer.js
@@ -7,13 +7,22 @@ var currentRound = 1;
 //Indicates if timer is paused
 var paused = false;
 
+//Durations for round and breaks
+var roundDuration = 1500;
+var shortBreakDuration = 300;
+var longBreakDuration = 900;
+
 $("document").ready(function(){
+    //update timer and buttin UIs
+    $("#timer").html(getCountdownString(roundDuration));
+    updateTimerButton(getStartButtonLabel(currentRound, getCountdownTime(currentRound)), true);
+
+    //Assign click event handlers
     $("#timerButton").click(function(){
         //If button has the "pause" label, set paused flag to true and update UI
         if($("#timerButton").html() === "Pause"){
             paused = true;
-            $("#timerButton").html("Resume");
-            $("#timerButton").removeClass("timerButtonPause").addClass("timerButtonStart");
+            updateTimerButton("Resume", true);
         }
         else{
             //if the pomodoroTimer is false, create and start a new timer.
@@ -23,9 +32,28 @@ $("document").ready(function(){
             else{
                 paused = false;
             }
-            $("#timerButton").html("Pause");
-            $("#timerButton").removeClass("timerButtonStart").addClass("timerButtonPause");
+            updateTimerButton("Pause", false);
         }
+    });
+
+    $("#settingsSaveButton").click(function(){
+        //update changes
+        updateDurations();
+
+        //reset the pomodoro timer
+        paused = false;
+        clearInterval(pomodoroTimer);
+        pomodoroTimer = false;
+        currentRound = 1;
+
+        //update button UI
+        updateTimerButton(getStartButtonLabel(currentRound, getCountdownTime(currentRound)), true);
+
+        //update timer ui
+        $("#timer").html(getCountdownString(roundDuration));
+
+        //hide modal
+        $("#settingsModal").modal('hide');
     });
 });
 
@@ -57,14 +85,13 @@ function startPomodoroTimer(){
             $("#timer").addClass("timerTimeUp");
 
             //update button UI
-            $("#timerButton").html(getStartButtonLabel(currentRound, getCountdownTime(currentRound)));
-            $("#timerButton").removeClass("timerButtonPause").addClass("timerButtonStart");
+            updateTimerButton(getStartButtonLabel(currentRound, getCountdownTime(currentRound)), true);
         }
     }, 1000);
 }
 
 function getStartButtonLabel(currentRound, countdown){
-    //every even numbered round is a short break.
+    //every even numbered round is a break.
     if(currentRound % 2 == 0){
         return "Start Break <br> (" + countdown/60 + " Minutes)";
     }
@@ -78,15 +105,15 @@ function getStartButtonLabel(currentRound, countdown){
 function getCountdownTime(currentRound){
     //every 8 rounds is a long break.
     if(currentRound % 8 == 0){ 
-        return 90;
+        return longBreakDuration;
     }
     //every even numbered round is a short break.
     else if(currentRound % 2 == 0){
-        return 30;
+        return shortBreakDuration;
     }
     //every odd numbered round is a normal round.
     else{
-        return 60;
+        return roundDuration;
     }
 }
 
@@ -99,3 +126,24 @@ function getCountdownString(time){
 
     return timeString;
 }
+
+//retrive values from duration inputs and update variables
+function updateDurations(){
+    roundDuration = $("#roundDurationInput").val() * 60;
+    shortBreakDuration = $("#shortBreakDurationInput").val() * 60;
+    longBreakDuration = $("#longBreakDurationInput").val() * 60;
+}
+
+//update timer button UI with label and styling
+function updateTimerButton(label, isPaused){
+    $("#timerButton").html(label);
+
+    //If the timer is paused, we want to add styling for the green start/resume button.
+    if(isPaused){
+        $("#timerButton").removeClass("timerButtonPause").addClass("timerButtonStart");
+    }
+    else{
+        $("#timerButton").removeClass("timerButtonStart").addClass("timerButtonPause");
+    }
+}
+
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Pomodoro Timer
-Simple Pomodoro Style Timer with a pattern of four 25 minute rounds with 5 minute breaks in between. Four rounds are followed by a 15 minute break.
+Simple Pomodoro Style Timer with a pattern of four 25 minute rounds with 5 minute breaks in between. Four rounds are followed by a 15 minute break. The duration for rounds and breaks are adjustable.
 
 # How to use
 This is a simple webpage which can be run on any browser. Clone the repository and open the `PomodoroTimer.html` file. Make sure that the `PomodoroTimer.js` file is in the same folder as `PomodoroTimer.html`.
